21067487f4cbd3ee3cade6549ea1754994ad42c8
canadrian72 <patterson.n.adrian@gmail.com>
2020-11-01 00:33:44 -0400

    Attempt at BLE service

--- SensAir Android App/app/src/main/java/com/example/sensair/MainActivity.java
+++ SensAir Android App/app/src/main/java/com/example/sensair/MainActivity.java
@@ -2,19 +2,35 @@ package com.example.sensair;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothSocket;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.IOException;
+import java.util.Set;
+import java.util.UUID;
+
 public class MainActivity extends AppCompatActivity
 {
 
+    private static final int REQUEST_ENABLE_BT = 1;
     protected Button buttonReadValues;
     protected TextView  eco2, tvoc, combustibleGas;
-    protected mBluetoothService btService;
+    protected BluetoothAdapter mBluetoothAdapter;
+    protected BtHelper btHelper = new BtHelper();
+    public static final String SerialPortUUID="0000dfb1-0000-1000-8000-00805f9b34fb";
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -27,10 +43,61 @@ public class MainActivity extends AppCompatActivity
 
     public void init()
     {
+//        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+//        if (bluetoothAdapter == null) {
+//            // Device doesn't support Bluetooth
+//        }
+//        if (!bluetoothAdapter.isEnabled()) {
+//            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+//            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+//        }
+//
+//        Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+//
+//        if (pairedDevices.size() > 0) {
+//            // There are paired devices. Get the name and address of each paired device.
+//            for (BluetoothDevice device : pairedDevices) {
+//                String deviceName = device.getName();
+//                String deviceHardwareAddress = device.getAddress(); // MAC address
+//            }
+//        }
+//        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+//        registerReceiver(receiver, filter);
+//
+//        BluetoothManager bluetoothManager = (BluetoothManager)getSystemService(Context.BLUETOOTH_SERVICE);
+//        BluetoothDevice mBluetoothDevice = bluetoothManager.getAdapter() .getRemoteDevice("0x20CD3987DD5D");
+
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null)
+        {// Device does not support Bluetooth
+        }
+        if (!mBluetoothAdapter.isEnabled())
+        {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, 1);
+        }
+
+        BluetoothDevice myDevice;
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+        if (pairedDevices.size() >0)
+        {
+            for (BluetoothDevice device : pairedDevices)
+            {
+               // mDevice = device;
+                System.out.println("----------------BLUETOOTH DEVICE FOUND:");
+                System.out.println(device.toString());
+                if(device.getAddress()=="20:CD:39:87:DD:5D")
+                    myDevice = device;
+            }
+        }
+
+
+
 
         tvoc = findViewById(R.id.tvocData);
         eco2 = findViewById(R.id.eco2Data);
         combustibleGas = findViewById(R.id.combustionData);
+        buttonReadValues = findViewById(R.id.buttonReadValues);
 
         buttonReadValues.setOnClickListener(new OnClickListener()
         {
@@ -41,10 +108,87 @@ public class MainActivity extends AppCompatActivity
                     * TODO Here we have to find out how to fetch data from dev board.
                     *       Once we have the data, just set text to readings (maybe process data too)
                  */
+                System.out.println("BUTTON CLICKED!!!");
                 eco2.setText("x ppm");
                 tvoc.setText("y (out of n)");
                 combustibleGas.setText("z ppm");
             }
         });
     }
+
+    private final BroadcastReceiver receiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Discovery has found a device. Get the BluetoothDevice
+                // object and its info from the Intent.
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+            }
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // Don't forget to unregister the ACTION_FOUND receiver.
+        unregisterReceiver(receiver);
+    }
+
+
+//    private class ConnectThread extends Thread {
+//        private final BluetoothSocket mmSocket;
+//        private final BluetoothDevice mmDevice;
+//
+//        public ConnectThread(BluetoothDevice device) {
+//            // Use a temporary object that is later assigned to mmSocket
+//            // because mmSocket is final.
+//            BluetoothSocket tmp = null;
+//            mmDevice = device;
+//
+//            try {
+//                // Get a BluetoothSocket to connect with the given BluetoothDevice.
+//                // MY_UUID is the app's UUID string, also used in the server code.
+//                tmp = device.createRfcommSocketToServiceRecord(UUID.fromString(SerialPortUUID));
+//            } catch (IOException e) {
+//               e.printStackTrace();
+//            }
+//            mmSocket = tmp;
+//        }
+//
+//        public void run() {
+//            // Cancel discovery because it otherwise slows down the connection.
+//            btHelper.cancelDiscovery();
+//
+//            try {
+//                // Connect to the remote device through the socket. This call blocks
+//                // until it succeeds or throws an exception.
+//                mmSocket.connect();
+//            } catch (IOException connectException) {
+//                // Unable to connect; close the socket and return.
+//                try {
+//                    mmSocket.close();
+//                } catch (IOException closeException) {
+//                    closeException.printStackTrace();
+//                }
+//                return;
+//            }
+//
+//            // The connection attempt succeeded. Perform work associated with
+//            // the connection in a separate thread.
+//            manageMyConnectedSocket(mmSocket);
+//        }
+//
+//        // Closes the client socket and causes the thread to finish.
+//        public void cancel() {
+//            try {
+//                mmSocket.close();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
+
+
 }
