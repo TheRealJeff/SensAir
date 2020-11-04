4de1f097057ee3678f957ff9a5312040bc957fbb
canadrian72 <patterson.n.adrian@gmail.com>
2020-11-04 14:41:20 -0500

    Arduino sketch now writes to bluetooth using HC-05.
    Make sure to check pin definitions and AT config. before trying to read.

--- Arduino Sketches/ArduinoUno_SensorReadSketch/ArduinoUno_SensorReadSketch.ino
+++ Arduino Sketches/ArduinoUno_SensorReadSketch/ArduinoUno_SensorReadSketch.ino
@@ -4,21 +4,18 @@
 
 #define CCS811_ADDR 0x5B    // Default I2C Address 
 #define MQ2_PIN A0          // Pins
-#define RX 2
-#define TX 3
+#define HC05_RX 10
+#define HC05_TX 11
 
 
-SoftwareSerial btSerial(RX,TX);    //  RX,TX     
+SoftwareSerial btSerial(HC05_RX,HC05_TX);    //  RX,TX     
 CCS811 ccs811(CCS811_ADDR);
 
 void setup()
 {
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, LOW);
-
   Serial.begin(9600); 
   btSerial.begin(38400);  //Baud Rate for AT-command Mode.  
-  Serial.println("|---------- Set Up ----------"); 
+  Serial.println("|---------- Set Up ----------|"); 
   
   while(!Serial){;}
   Wire.begin(); //  Inialize I2C 
@@ -26,44 +23,44 @@ void setup()
   if (ccs811.begin() == false)
   {
     Serial.print("CCS811 error. Please check wiring.");
-    while (1)
-      ;
   }
-  
 }
 
 void loop()
 {
   if (ccs811.dataAvailable())
   {
-    ccs811.readAlgorithmResults();    // read results
-    int mq2_reading = analogRead(MQ2_PIN);  
+    ccs811.readAlgorithmResults();    // read results from CSS811
+    int mq2_reading = analogRead(MQ2_PIN);    // read results from mq2
 
     int co2 = ccs811.getCO2();
-    Serial.print(co2);    // print co2 reading
-    Serial.print("C");
-    btSerial.print(co2);   
-    btSerial.print("C");
+
+    btSerial.write(co2);   
+    btSerial.write("C");
+    Serial.print(co2);
+    Serial.println(" CO2 Measured");
 
     int tvoc = ccs811.getTVOC();   // print tvoc reading
-    Serial.print(tvoc);   
-    Serial.print("T");
-    btSerial.print(tvoc);   
-    btSerial.print("T");
 
-    Serial.print(mq2_reading);    // print mq2 reading
-    Serial.print("M");
-    btSerial.print(mq2_reading);
-    btSerial.print("M");
-;
-  }
+    btSerial.write(tvoc);   
+    btSerial.write("T");
+    Serial.print(tvoc);
+    Serial.println(" TVOC Measured");
+
     
- 
-// if (btSerial.available())       // uncomment to enter AT command mode 
-//   Serial.write(btSerial.read()); 
-// if (Serial.available()) 
-//   btSerial.write(Serial.read());
+    btSerial.write(mq2_reading);
+    btSerial.write("M");
+    Serial.print(mq2_reading);
+    Serial.println(" MQ2 ppm Measured");
 
+  }
+
+//  if (btSerial.available())       // UNCOMMENT to allow for AT commands. Also uncomment all else in loop()
+//    Serial.write(btSerial.read());
+//
+//  
+//  if (Serial.available())
+//    btSerial.write(Serial.read());
   
   delay(100); //Don't spam the I2C bus
 }
