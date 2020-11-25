7a1934da3d9e065b0dcf572f39f1bddb5bc8e910
Jeffrey <jeffrey.allan.hamel@gmail.com>
2020-11-24 21:27:22 -0500

    added pressure and altitude threshold and toggle switch settings

--- SensAirAndroidApp/app/src/main/java/com/example/sensair/ProfileActivity.java
+++ SensAirAndroidApp/app/src/main/java/com/example/sensair/ProfileActivity.java
@@ -1,11 +1,15 @@
 package com.example.sensair;
 
 import android.os.Bundle;
+import android.text.InputType;
+import android.widget.EditText;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.NavUtils;
+import androidx.preference.EditTextPreference;
 import androidx.preference.PreferenceFragmentCompat;
 
 public class ProfileActivity extends AppCompatActivity {
@@ -31,6 +35,23 @@ public class ProfileActivity extends AppCompatActivity {
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
+
+            EditTextPreference editTextPreferenceAltitude = findPreference("altitude");
+            editTextPreferenceAltitude.setOnBindEditTextListener(new EditTextPreference.OnBindEditTextListener() {
+                @Override
+                public void onBindEditText(@NonNull EditText editText) {
+                    editText.setInputType(InputType.TYPE_CLASS_NUMBER);
+                }
+            });
+
+            EditTextPreference editTextPreferenceTemperature = findPreference("temperature");
+            editTextPreferenceTemperature.setOnBindEditTextListener(new EditTextPreference.OnBindEditTextListener() {
+                @Override
+                public void onBindEditText(@NonNull EditText editText) {
+                    editText.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_SIGNED);
+                }
+            });
+
         }
     }
 
